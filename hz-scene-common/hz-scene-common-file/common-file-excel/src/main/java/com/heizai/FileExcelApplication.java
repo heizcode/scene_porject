4937d9a0bdac61f3c9b786120de69ef9a96f0846
zhulx <>
2023-03-14 15:55:06 +0800

    [新增] - 文件相关架构模块构建 [新增] - springboot基本pom文件引入

---
+++ hz-scene-common/hz-scene-common-file/common-file-excel/src/main/java/com/heizai/FileExcelApplication.java
@@ -0,0 +1,26 @@
+package com.heizai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhulx
+ * @version 1.0
+ * @ClassName com.heizai.FileExcelApplication
+ * @Description TODO
+ * @Date 2023/3/14 11:22
+ * @since JDK1.8
+ */
+@SpringBootApplication
+public class FileExcelApplication {
+
+    /**
+     * 主启动类
+     * 遇到的问题记录：https://blog.csdn.net/M_Love_U/article/details/123469003
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(FileExcelApplication.class);
+    }
+
+}
